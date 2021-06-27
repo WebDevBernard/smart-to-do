94de5f40e20853f4865d1d0dfeecdbbdb8df3b8d
cyn <cuixintong@alumni.ubc.ca>
2021-06-27 14:27:11 -0700

    ajax stuff

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -11,9 +11,9 @@ $(document).ready(function() {
 
   // renders the timeline in reverse-chronological order
   const renderTasks = function(tasks) {
-    $('.tasks-timeline').empty();
+    $('.category-card').empty();
       tasks.forEach(function(tweet) {
-        createTaskElement(task).appendTo('.tasks-timeline');
+        createTaskElement(task).appendTo('.category-card');
       });
   }
 
@@ -21,7 +21,7 @@ $(document).ready(function() {
   const loadTasks = function() {
     $.ajax({
       method: 'GET',
-      url: '/',
+      url: '/tasks/:id',
       data: $('#tasksform').serialize(),
       dataType: 'json',
       success: function (data) {
