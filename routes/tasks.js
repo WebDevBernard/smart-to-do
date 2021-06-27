c55891910ece6a9e4d5ba5620db5f0fac3505139
Bernard Yang <ybernard@gmail.com>
2021-06-27 17:33:26 +0000

    add put post delete routes for tasks PP yucen and bernard

--- routes/tasks.js
+++ routes/tasks.js
@@ -5,7 +5,8 @@ module.exports = (db) => {
   router.get('/', (req, res) => {
     db.query(`SELECT * FROM tasks`)
       .then(data => {
-        console.log(data);
+        const response = data.rows;
+        res.json({ response });
       })
       .catch(err => {
         res
@@ -14,9 +15,11 @@ module.exports = (db) => {
       });
   });
   router.get('/:id', (req, res) => {
-    db.query(`SELECT * FROM tasks WHERE user_id = $1;`, [req.params.id])
+    const userId = req.params.id;
+    db.query(`SELECT * FROM tasks WHERE user_id = $1;`, [userId])
       .then(data => {
-        res.json(data.rows);
+        const response = data.rows;
+        res.json({ response });
       })
       .catch(err => {
         res
@@ -24,17 +27,47 @@ module.exports = (db) => {
           .json({ error: err.message });
       });
   });
-  router.post('/', (req, res) => {
-    db.query(`INSERT INTO tasks (user_id, name, category_name, date_created) VALUES ($1, $2, $3, $4);`, [1, "watch watchmen", "to-watch", "Now()"])
+  router.post('/:id', (req, res) => {
+    const userTask = req.body;
+    const userId = req.params.id;
+    db.query(`INSERT INTO tasks (user_id, name, category_name, date_created) VALUES ($1, $2, $3, $4);`, [userId, userTask, "to-watch", "Now()"])
       .then(data => {
-        const response = {data.rows};
-        res.json(response);
+        const response = data.rows;
+        console.log({ response });
       })
       .catch(err => {
         res
           .status(500)
           .json({ error: err.message });
       });
+  });
+  router.put('/:id', (req, res) => {
+    const userTask = req.body;
+    const userId = req.params.id;
+    db.query(`UPDATE tasks
+    SET name = 'Joe',
+    user_id = 1,
+    category_name = 'to-watch',
+    date_created = 'NOW()'
+    WHERE id = 1 AND user_id = 1
+    RETURNING *;`)
+      .then(data => {
+        const response = data.rows;
+        console.log({ response });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+  router.delete('/', (req, res) => {
+    db.query(`DELETE FROM tasks WHERE user_id = $1 AND id = $2;`, [1, 3]);
+    res.json("Your Task Has Been Deleted");
   });
   return router;
 };
+
+
+
+
