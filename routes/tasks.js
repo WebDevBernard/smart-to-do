7d32f20948fe0fefeac2733da814120670ae5ac2
Bernard Yang <ybernard@gmail.com>
2021-06-27 15:24:23 +0000

    add tasks post routes

--- routes/tasks.js
+++ routes/tasks.js
@@ -27,7 +27,7 @@ module.exports = (db) => {
   router.post('/', (req, res) => {
     db.query(`INSERT INTO tasks (user_id, name, category_name, date_created) VALUES ($1, $2, $3, $4);`, [1, "watch watchmen", "to-watch", "Now()"])
       .then(data => {
-        const { response } = data.rows;
+        const response = {data.rows};
         res.json(response);
       })
       .catch(err => {
