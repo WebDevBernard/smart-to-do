2c9e9de4f360928f74f1e4a75a73c3b39ec546c2
cyn <cynthiacui98@gmail.com>
2021-06-28 16:27:34 -0700

    updates app.js with some of andy's material

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,78 +1,92 @@
 
 $(() => {
-  // $.ajax({
-  //   method: "GET",
-  //   url: "/api/users"
-  // }).done((users) => {
-  //   for(user of users) {
-  //     $("<div>").text(user.name).appendTo($(".tasks-timeline"));
-  //   }
-  // });
+  $.ajax({
+    method: "GET",
+    url: "/api/users"
+  }).done((users) => {
+    for(user of users) {
+      $("<div>").text(user.name).appendTo($(".tasks-timeline"));
+    }
+  });
 
-  // $.ajax({
-  //   method: 'GET',
-  //   url: '/tasks/:id',
-  //   data: $('#tasksform').serialize(),
-  //   dataType: 'json',
-  // }).done((tasks) => {
-  //   for(task of tasks) {
-  //     $("<section class='to-watch-list'></section>").text(task.name).appendTo($("<div class='category-card'></div>"));
-  //   }
-  // });
+  $.ajax({
+    method: 'GET',
+    url: '/tasks/:id',
+    data: $('#tasksform').serialize(),
+    dataType: 'json',
+  }).done((tasks) => {
+    for(task of tasks) {
+      $("<section class='to-watch-list'></section>").text(task.name).appendTo($("<div class='category-card'></div>"));
+    }
+  });
 
-  // $.ajax({
-  //   method:'GET',
-  //   url: "/tasks",
-  //   success: (data) => {
-  //     for (task of data) {
-  //       const result=`<td>${task[1].name}</td>`
-  //       $("#result").html(result);
-  //     }
-  //   },
-  // });
+  $.ajax({
+    method:'GET',
+    url: "/tasks",
+    success: (data) => {
+      for (task of data) {
+        const result=`<td>${task[1].name}</td>`
+        $("#result").html(result);
+      }
+    },
+  });
 
 
   // // creating the list article element
-  // const createTaskElement = function({task: {id, name, category_name, date_created}}) {
-  //   const $task = $(`<article class="tweet-container"></article>`);
-  //   const $categoryCard = $(`<div class="category-card></div>`).appendTo($task);
-  //   const $categoryName = $(`<h5>${category_name}</h5>`).appendTo($categoryCard);
-  //   const $text = $(`<ul><li>${name}</li><ul>`).text(content.text).appendTo($categoryCard);
-
-  //   return $task;
-  // };
+  const createTaskElement = (task) {
+    const $task = $(`
+    <div class="category-card">
+      <h5>${task.category_name}</h5>
+      <ul>
+      <li>${task.name}</li>
+      </ul>
+    </div>
+    `);
+  };
 
   // // renders the timeline in reverse-chronological order
-  // const renderTasks = function(tasks) {
-  //   $('.category-card').empty();
-  //     tasks.forEach(function(tweet) {
-  //       createTaskElement(task).appendTo('.category-card');
-  //     });
-  // }
+  const renderTasks = function(tasks) {
+    // $('.category-card').empty();
+    //   tasks.forEach(function(task) {
+    //     createTaskElement(task).appendTo('.category-card');
+    //   });
+
+    const $taskList = $('.category-card');
+    $taskList.empty();
+
+    for (const task of tasks) {
+      $taskList.append(createTaskElement(task));
+    }
+  }
 
   // // using ajax to load the tasks
-  // const loadTasks = function() {
-  //   $.ajax({
-  //     method: 'GET',
-  //     url: '/tasks/:id',
-  //     data: $('#tasksform').serialize(),
-  //     dataType: 'json',
-  //     success: function (data) {
-  //       renderTasks(data);
-  //     }
-  //   });
-  // };
+  const loadTasks = () => {
+    // $.ajax({
+    //   method: 'GET',
+    //   url: '/tasks/:id',
+    //   data: $('#tasksform').serialize(),
+    //   dataType: 'json',
+    //   success: function (data) {
+    //     renderTasks(data);
+    //   }
+    // });
 
-  // loadTasks;
+    $.get('/tasks')
+      .then((tasks) => {
+        renderTasks(tasks.reverse());
+      });
+  };
+
+  loadTasks();
 
   // using ajax to submit the tasks
-  const postTweet = function(data) {
+  const postTasks = function(data) {
     $.ajax({
       method: 'POST',
       data: data,
       url: '/tasks',
 
-      success: function (result) {
+      success: () => {
         loadTasks();
         $('.add-task textarea').val('');
       },
@@ -90,7 +104,7 @@ $(() => {
 //  console.log($('#text-box').val().replace("text=", "").serialize());
     let data = { data :$('#text-box').val().replace("text=", "")};
     // console.log(data)
-    postTweet(data);
+    postTasks(data);
 
 
     // clears task area
