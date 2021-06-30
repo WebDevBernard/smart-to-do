1c857dd8e2b565ea49e5d1f63d8dcf10e7e63cb6
cyn <cynthiacui98@gmail.com>
2021-06-29 18:07:59 -0700

    working delete route

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,14 +1,20 @@
 $(() => {
   // creating the list article element
   const createTaskElement = (task) => {
-    const newTask = `
-      <li>${task.name}</li>`;
+    const $newTask = $(`
+    <li>${task.name}
+    </li>`);
 
-    return $(newTask);
+    const $deleteButton = $(`<button class="deletetask" type="submit" taskid="${task.id}"><i class="far fa-trash-alt"></i>
+    </button>`);
+
+    $newTask.append($deleteButton);
+
+    return $newTask;
   };
-  
+
   // // renders the tasks in reverse-chronological order
-  const renderTasks = function(tasks) {
+  const renderTasks = function (tasks) {
     // const $taskList = $("<div class='cards'></div>");
     const $towatch = $("#towatch");
     const $toread = $("#toread");
@@ -20,7 +26,6 @@ $(() => {
     $tobuy.empty();
     $toeat.empty();
 
-    
     for (const task of tasks) {
       if (task.category_name === "to-watch") {
         $towatch.append(createTaskElement(task));
@@ -39,42 +44,38 @@ $(() => {
 
   // using ajax to load the tasks
   const loadTasks = () => {
-    // $.get('/tasks')
-    //   .then((tasks) => {
-    //     renderTasks(tasks.reverse());
-    //   });
-    // $.ajax({
-    //   method: "GET",
-    //   url: "/tasks",
-    //   data: $("#text-box").text(),
-    //   dataType: "json",
-    //   success: function(data) {
-    //     renderTasks(data);
-    //   },
-    // });
-
     $.ajax({
-      method: 'GET',
+      method: "GET",
       url: "/tasks",
       success: (data) => {
         renderTasks(data);
-        // const result = [];
-        // for (const task of data) {
-        //   result.push(`<li>${task.name}</li>`);
-        //   $(`#${task.category_name}`).html(result);
-        // }
       },
     });
   };
   loadTasks();
 
+  // const deleteTask = function(event) {
+  //   const taskElm = $(this).parents('li');
+  //   const options = {
+  //     method: 'DELETE',
+  //     url: `/tasks/${taskElm.data('id')}`,
+  //   };
+  //   $.ajax(options)
+  //     .done(function(res) {
+  //       taskElm.remove();
+  //     })
+  //     .fail(function(err) {
+  //       console.error('Failed to remove task from DB', err);
+  //     });
+  // };
+
   const $newTextBox = $("#textbox");
 
   // event listener for submit
-  $('.taskbutton').on("click", function(event) {
+  $(".taskbutton").on("click", function (event) {
     event.preventDefault();
     console.log($newTextBox.val());
-    const data = {name: $newTextBox.val()};
+    const data = { name: $newTextBox.val() };
     // add category_name to data object & have api use the data.category_name
     // const data = $(this).val();
     // posting the new task
@@ -83,7 +84,7 @@ $(() => {
       url: "/tasks",
       data: data,
 
-      success: function(result) {
+      success: function (result) {
         loadTasks();
         $("#textbox").val("");
       },
@@ -92,4 +93,24 @@ $(() => {
       },
     });
   });
+
+  // event listener for delete
+  $(".cards").on("click", ".deletetask", function (event) {
+    event.preventDefault();
+    const data = loadTasks();
+
+    $.ajax({
+      method: "DELETE",
+      url: `tasks/${$(this).attr("taskid")}`,
+      data: data,
+
+      success: function (result) {
+        loadTasks();
+      },
+      error: function (err) {
+        console.log("error:", err);
+      },
+    });
+  });
+
 });
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,75 +1,70 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 // const sortable = require('../scripts/sortable.js');
 
 module.exports = (db) => {
-  router.get('/', (req, res) => {
+  router.get("/", (req, res) => {
     db.query(`SELECT * FROM tasks`)
-      .then (data => {
+      .then((data) => {
         const response = data.rows;
         res.json(response);
       })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
       });
   });
-  router.get('/:id', (req, res) => {
+  router.get("/:id", (req, res) => {
     const userId = parseInt(req.params.id);
     db.query(`SELECT * FROM tasks WHERE user_id = $1;`, [userId])
-      .then(data => {
+      .then((data) => {
         const response = data.rows;
         res.json({ response });
       })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
       });
   });
 
-  router.post('/', (req, res) => {
+  router.post("/", (req, res) => {
     const name = req.body.name;
-    db.query(`INSERT INTO tasks (user_id, name, category_name, date_created) VALUES ($1, $2, $3, $4);`, [1, name, "to-watch", "Now()"])
-      .then(data => {
-        res.json({ data });
-      });
-
+    db.query(
+      `INSERT INTO tasks (user_id, name, category_name, date_created) VALUES ($1, $2, $3, $4);`,
+      [1, name, "to-watch", "Now()"]
+    ).then((data) => {
+      res.json({ data });
+    });
   });
 
-  router.put('/:id', (req, res) => {
+  router.put("/:id", (req, res) => {
     const userTask = req.body;
     const userId = req.params.id;
-    db.query(`UPDATE tasks
+    db.query(
+      `UPDATE tasks
     SET name = 'Joe',
     user_id = 1,
     category_name = 'to-watch',
     date_created = 'NOW()'
     WHERE id = 1 AND user_id = 1
-    RETURNING *;`)
-      .then(data => {
+    RETURNING *;`
+    )
+      .then((data) => {
         const response = data.rows;
         console.log({ response });
       })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
       });
   });
-  router.delete('/', (req, res) => {
-    if (sortable.removeOnSpill) {
-      db.query(`DELETE FROM tasks WHERE user_id = $1 AND id = $2;`, [1, 3]);
-      res.json("Your Task Has Been Deleted");
-    } else {
-      return;
-    }
-    
+
+
+  console.log("this is outside the delete route");
+
+  router.delete(`/:id`, (req, res) => {
+    const taskId = parseInt(req.params.id);
+    console.log("this is inside the delete route");
+    db.query(`DELETE FROM tasks WHERE id = $1;`, [taskId]
+    );
+    res.json("Your Task Has Been Deleted");
   });
   return router;
 };
-
-
-
-
