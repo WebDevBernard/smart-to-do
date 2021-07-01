7c7d6150cab51a5ea344867805e7cfc7e2f5d039
Yucen Liu <yucen.u@gmail.com>
2021-07-01 07:35:55 +0000

    working APIs for watch-list && read-list

--- routes/tasks.js
+++ routes/tasks.js
@@ -58,48 +58,34 @@ module.exports = (db) => {
         res.json({ data });
       });
     } else {
-      console.log("this is inside the else statement");
-      if (movieCat(name)) {
-        db.query(
-          `INSERT INTO tasks (user_id, name, category_name, date_created) VALUES ($1, $2, $3, $4);`,
-          [1, name, "to-watch", "Now()"]
-        ).then((data) => {
-          res.json({ data });
-        });
-      }
-
-      if (getBooks(name)) {
-        console.log("ROHIT GET BOOKS NAME");
-        db.query(
-          `INSERT INTO tasks (user_id, name, category_name, date_created) VALUES ($1, $2, $3, $4);`,
-          [1, name, "to-read", "Now()"]
-        ).then(() => {
-          res.status(200);
-        });
-      }
+      movieCat(name).then((isMovie) => {
+        if (isMovie) {
+          db.query(
+            `INSERT INTO tasks (user_id, name, category_name, date_created) VALUES ($1, $2, $3, $4);`,
+            [1, name, "to-watch", "Now()"]
+          ).then((data) => {
+            res.status(200);
+            res.json({ data });
+          });
+        } else {
+          getBooks(name).then((isBook) => {
+            if (isBook) {
+              db.query(
+                `INSERT INTO tasks (user_id, name, category_name, date_created) VALUES ($1, $2, $3, $4);`,
+                [1, name, "to-read", "Now()"]
+              ).then((data) => {
+                res.status(200);
+                res.json({ data });
+              });
+            } else {
+              res.status(400);
+              res.json("todo: write proper error msg")
+            }
+          })
+        }
+      })
     }
   });
-
-  // router.put("/:id", (req, res) => {
-  //   const userTask = req.body;
-  //   const userId = req.params.id;
-  //   db.query(
-  //     `UPDATE tasks
-  // SET name = 'Joe',
-  // user_id = 1,
-  // category_name = 'to-watch',
-  // date_created = 'NOW()'
-  // WHERE id = 1 AND user_id = 1
-  // RETURNING *;`
-  //   )
-  //     .then((data) => {
-  //       const response = data.rows;
-  //       console.log({ response });
-  //     })
-  //     .catch((err) => {
-  //       res.status(500).json({ error: err.message });
-  //     });
-  // });
 
   router.delete(`/:id`, (req, res) => {
     const taskId = parseInt(req.params.id);
--- server.js
+++ server.js
@@ -23,7 +23,7 @@ db.connect();
 app.use(morgan('dev'));
 
 app.set("view engine", "ejs");
-// app.use(express.json());
+app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/styles", sass({
   src: __dirname + "/styles",
